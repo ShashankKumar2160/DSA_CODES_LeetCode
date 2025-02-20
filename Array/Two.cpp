13fbac3cd6414de27cef37ebcf1849058d97521b
ShashankKumar2160 <shashankkumar2160@gmail.com>
2025-02-20 23:45:42 +0530

    first

--- Array/Two.cpp
+++ Array/Two.cpp
@@ -1,1 +1,18 @@
-//
+class Solution {
+    public:
+        vector<int> twoSum(vector<int>& nums, int target) {
+            unordered_map<int,int>mpp;
+            vector<int>res;
+            int n = nums.size();
+            
+            for(int i = 0; i<n; i++){
+                if(mpp.find(target - nums[i]) != mpp.end()){
+                    res.push_back(i);
+                    res.push_back(mpp[target - nums[i]]);
+                    return res;
+                }
+                mpp[nums[i]] = i;
+            }
+            return res;
+        }
+    };
